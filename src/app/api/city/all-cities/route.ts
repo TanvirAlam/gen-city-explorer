e67bb07d1422ee9c651ac0a6f243f3c6918741e5
Tanvir Alam <tanvir.alam.shawn@gmail.com>
2024-10-31 14:02:10 +0100

    Adding route for all-cities

---
+++ src/app/api/city/all-cities/route.ts
@@ -0,0 +1,10 @@
+import addresses from '../../../../utils/data/address.json';
+
+export async function GET() {
+  return new Response(JSON.stringify(addresses.cities), {
+    status: 200,
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  });
+}
